b3562d2de89d522ba14ad5598af2cf6ab1f17329
lichaozheng <lcz@ejoy.com>
2014-09-14 15:03:38 +0800

    render sea

--- src/dawn_scene.lua
+++ src/dawn_scene.lua
@@ -2,29 +2,48 @@ local ej = require "ejoy2d"
 
 local sw = 768
 local sh = 1024
+-- local sw = 320
+-- local sh = 568
 
 local SKY_PCT = 0.6
 
-local SKY_TEX_W = 2
-local SKY_TEX_H = 100
+local SKY_TEX_W = 1
+local SKY_TEX_H = 1
+local SEA_TEX_W = 64
+local SEA_TEX_H = 64
 
 local M = {}
 
 function M:init()
-	self.v_sky = ej.sprite("dawn", "sky")
+	self.v_sky = ej.sprite("dawn", "blank")
 	self.v_sky.program = "sky"
-	self.v_sky.program_param.top = { 0.537, 0.725, 0.816, 1 }
-	self.v_sky.program_param.bottom = { 0.792, 0.843, 0.859, 1 }
+	self.v_sky.program_param.far = { 0.17, 0.49, 0.71, 1 }
+	self.v_sky.program_param.near = { 0.60, 0.78, 0.88, 1 }
 
 	self.v_sky:ps(sw/2, sh*SKY_PCT/2)
 	self.v_sky:sr(sw/SKY_TEX_W, sh*SKY_PCT/SKY_TEX_H)
+
+	self.v_sea = ej.sprite("dawn", "noise")
+	self.v_sea.program = "sea"
+	self.v_sea.program_param.far = { 0.70, 0.82, 0.91, 1 }
+	self.v_sea.program_param.near = { 0.06, 0.38, 0.53, 1 }
+	self.v_sea.program_param.spec = { 0.80, 0.80, 0.80, 1 }
+
+	self.v_sea:ps(sw/2, sh*(1+SKY_PCT)/2)
+	self.v_sea:sr(sw/SEA_TEX_W, sh*(1-SKY_PCT)/SEA_TEX_H)
+
+	self.v_time = 0
 end
 
 function M:update()
 end
 
 function M:draw()
+	self.v_time = self.v_time + 0.05
+	self.v_sea.program_param.t = self.v_time
+
 	self.v_sky:draw()
+	self.v_sea:draw()
 end
 
 return M
--- src/dawn_shader.lua
+++ src/dawn_shader.lua
@@ -17,22 +17,51 @@ void main() {
 
 local sky_fs = [[
 uniform sampler2D texture0;
-uniform vec4 top;
-uniform vec4 bottom;
+uniform vec4 far;
+uniform vec4 near;
 
 varying vec2 v_texcoord;
 varying vec4 v_color;
 
 void main() {
-	float f = texture2D(texture0, v_texcoord).r;
-	gl_FragColor = mix(top, bottom, f);
+	float f = pow(v_texcoord.y, 0.3);
+	gl_FragColor = mix(far, near, f);
+}
+]]
+
+local sea_fs = [[
+uniform sampler2D Texture0;
+uniform float t;
+uniform vec4 far;
+uniform vec4 near;
+uniform vec4 spec;
+
+varying vec2 v_texcoord;
+varying vec4 v_color;
+
+void main(void)
+{
+	vec2 tex_scale = vec2(0.5, 15.0);
+	vec2 noise_speed = vec2(0.005, -0.08);
+
+	vec2 tc = vec2(v_texcoord.x, pow(v_texcoord.y, 0.1));
+	vec2 nc = tc*tex_scale + t*noise_speed;
+
+	vec4 noise = texture2D(Texture0, nc);
+	float n = mix(noise.x, noise.y, (sin(t) + 1.0) * 0.5);
+	float w = (sin(tc.y*80.0 + n - t) + 1.0) * 0.5;
+
+	vec4 base1 = near * (1.0 + n*0.1);
+	vec4 base2 = mix(spec, base1, pow(w - n*0.1, 0.15));
+	gl_FragColor = mix(far, base2, pow(tc.y, 3.0));
 }
 ]]
 
 local M = {}
 
 function M.init()
-	shader.load("sky", sky_fs, vs, {top="4f", bottom="4f"})
+	shader.load("sky", sky_fs, vs, {far="4f", near="4f"})
+	shader.load("sea", sea_fs, vs, {t="1f", far="4f", near="4f", spec="4f"})
 end
 
 return M
