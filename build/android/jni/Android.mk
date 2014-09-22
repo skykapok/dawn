afffef0ec8be5aae0b0cbc55e6e09942cec661cc
lichaozheng <lcz@ejoy.com>
2014-09-22 12:25:53 +0800

    add ndk mk files

---
+++ build/android/jni/Android.mk
@@ -0,0 +1,67 @@
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := ejoy2d
+
+LOCAL_SRC_FILES := main.c
+
+LOCAL_SRC_FILES += \
+                   ../../../engine/lib/dfont.c \
+                   ../../../engine/lib/ejoy2dgame.c \
+                   ../../../engine/lib/fault.c \
+                   ../../../engine/lib/label.c \
+                   ../../../engine/lib/lmatrix.c \
+                   ../../../engine/lib/lparticle.c \
+                   ../../../engine/lib/lshader.c \
+                   ../../../engine/lib/lsprite.c \
+                   ../../../engine/lib/matrix.c \
+                   ../../../engine/lib/particle.c \
+                   ../../../engine/lib/ppm.c \
+                   ../../../engine/lib/scissor.c \
+                   ../../../engine/lib/screen.c \
+                   ../../../engine/lib/shader.c \
+                   ../../../engine/lib/sprite.c \
+                   ../../../engine/lib/spritepack.c \
+                   ../../../engine/lib/texture.c
+
+LOCAL_SRC_FILES += \
+                   ../../../engine/lua/lapi.c \
+                   ../../../engine/lua/lauxlib.c \
+                   ../../../engine/lua/lbaselib.c \
+                   ../../../engine/lua/lbitlib.c \
+                   ../../../engine/lua/lcode.c \
+                   ../../../engine/lua/lcorolib.c \
+                   ../../../engine/lua/lctype.c \
+                   ../../../engine/lua/ldblib.c \
+                   ../../../engine/lua/ldebug.c \
+                   ../../../engine/lua/ldo.c \
+                   ../../../engine/lua/ldump.c \
+                   ../../../engine/lua/lfunc.c \
+                   ../../../engine/lua/lgc.c \
+                   ../../../engine/lua/linit.c \
+                   ../../../engine/lua/liolib.c \
+                   ../../../engine/lua/llex.c \
+                   ../../../engine/lua/lmathlib.c \
+                   ../../../engine/lua/lmem.c \
+                   ../../../engine/lua/loadlib.c \
+                   ../../../engine/lua/lobject.c \
+                   ../../../engine/lua/lopcodes.c \
+                   ../../../engine/lua/loslib.c \
+                   ../../../engine/lua/lparser.c \
+                   ../../../engine/lua/lstate.c \
+                   ../../../engine/lua/lstring.c \
+                   ../../../engine/lua/lstrlib.c \
+                   ../../../engine/lua/ltable.c \
+                   ../../../engine/lua/ltablib.c \
+                   ../../../engine/lua/ltm.c \
+                   ../../../engine/lua/lundump.c \
+                   ../../../engine/lua/lvm.c \
+                   ../../../engine/lua/lzio.c
+
+LOCAL_C_INCLUDES := $(LOCAL_PATH) \
+                    $(LOCAL_PATH)/../../../engine/lib \
+                    $(LOCAL_PATH)/../../../engine/lua \
+
+LOCAL_LDLIBS := -llog -landroid -lGLESv2
+
+include $(BUILD_SHARED_LIBRARY)
